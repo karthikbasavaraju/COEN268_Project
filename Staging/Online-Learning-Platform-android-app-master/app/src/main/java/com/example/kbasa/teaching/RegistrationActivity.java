0f33e60c4ed6d68ccb044bce9be6c6a006eec165
Abhiman Kolte <abhimanbhau@icloud.com>
2018-03-17 22:40:15 -0700

    Fix crash on select profile picture

--- Staging/Online-Learning-Platform-android-app-master/app/src/main/java/com/example/kbasa/teaching/RegistrationActivity.java
+++ Staging/Online-Learning-Platform-android-app-master/app/src/main/java/com/example/kbasa/teaching/RegistrationActivity.java
@@ -1,9 +1,12 @@
 package com.example.kbasa.teaching;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -388,6 +391,14 @@ public class RegistrationActivity extends AppCompatActivity {
 
     public void profilePic(View v) {
         Button imageView = findViewById(R.id.btn_profilePic);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if(ActivityCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE}, 100);
+                return;
+            }
+        }
 
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
